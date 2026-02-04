5a2c3e4249f662235e23462aeaa5a7138374dc36
djawedch <120687949+djawedch@users.noreply.github.com>
2026-02-04 19:07:42 +0100

    Small Modifications

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,16 +3,24 @@
 namespace App\Http\Controllers;
 
 use App\Models\Job;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class SearchController extends Controller
 {
-    public function __invoke()
+    public function __invoke(Request $request): View
     {
+        $searchTerm = $request->input('q');
+
+        if (!$searchTerm) {
+            return view('results', ['jobs' => collect()]);
+        }
+
         $jobs = Job::query()
             ->with(['employer', 'tags'])
-            ->where('title', 'LIKE', '%' . request('q') . '%')
+            ->where('title', 'LIKE', "%{$searchTerm}%")
             ->get();
 
-        return view('results', ['jobs' => $jobs]);
+        return view('results', compact('jobs'));
     }
 }
