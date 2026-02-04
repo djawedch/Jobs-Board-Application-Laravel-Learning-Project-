e5163347697f2d995bd1d1974e670c24793ed750
djawedch <120687949+djawedch@users.noreply.github.com>
2026-02-04 23:17:32 +0100

    Small Modifications

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Models\Tag;
+use Illuminate\View\View;
 
 class TagController extends Controller
 {
-    public function __invoke(Tag $tag)
+    public function __invoke(Tag $tag): View
     {
+        $tag->load(['jobs.employer', 'jobs.tags']);
+        
         return view('results', ['jobs' => $tag->jobs]);
     }
 }
