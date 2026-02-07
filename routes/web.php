9c0e01924b142953d9cd0a8b0a17559c93ca0c4f
djawedch <120687949+djawedch@users.noreply.github.com>
2026-02-07 23:20:48 +0100

    Small Modifications

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\{JobController, SessionController, RegisteredUserController, SearchController, TagController};
 
-Route::get('/', [JobController::class, 'index'])->name('jobs.index');
-
-Route::middleware('auth')->group(function () {
-    Route::prefix('jobs')->controller(JobController::class)->group(function () {
-        Route::get('/create', 'create')->name('jobs.create');
-        Route::post('/', 'store')->name('jobs.store');
-        Route::get('/{job}/edit', 'edit')->name('jobs.edit');
-        Route::patch('/{job}', 'update')->name('jobs.update');
-        Route::get('/{job}', 'show')->name('jobs.show');
-        Route::delete('/{job}', 'destroy')->name('jobs.destroy');
-    });
-
-    Route::delete('/logout', [SessionController::class, 'destroy'])->name('logout');
+Route::controller(JobController::class)->group(function () {
+    Route::get('/', 'index')->name('jobs.index');
+    Route::get('/jobs/{job}', 'show')->name('jobs.show');
 });
+
+Route::get('/search', SearchController::class)->name('search');
+Route::get('/tags/{tag:name}', TagController::class)->name('tags.show');
 
 Route::middleware('guest')->group(function () {
     Route::controller(RegisteredUserController::class)->group(function () {
@@ -30,5 +23,14 @@ Route::middleware('guest')->group(function () {
     });
 });
 
-Route::get('/search', SearchController::class)->name('search');
-Route::get('/tags/{tag:name}', TagController::class)->name('tags.show');
+Route::middleware('auth')->group(function () {
+    Route::prefix('jobs')->controller(JobController::class)->group(function () {
+        Route::get('/create', 'create')->name('jobs.create');
+        Route::post('/', 'store')->name('jobs.store');
+        Route::get('/{job}/edit', 'edit')->name('jobs.edit');
+        Route::patch('/{job}', 'update')->name('jobs.update');
+        Route::delete('/{job}', 'destroy')->name('jobs.destroy');
+    });
+
+    Route::delete('/logout', [SessionController::class, 'destroy'])->name('logout');
+});
